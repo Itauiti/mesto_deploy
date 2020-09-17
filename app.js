51b466aa5aa2151a4e634421813ddd7c22f62db0
Irina Isaeva <64747080+Itauiti@users.noreply.github.com>
2020-09-17 14:47:28 +0300

    убран тест на проверку падения сервера

--- app.js
+++ app.js
@@ -35,11 +35,6 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(limiter);
 app.use(requestLogger);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
